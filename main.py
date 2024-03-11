25fde0226197d04157201907115ef27822c0bf35
Irina Kirillova <keira.kirillova@gmail.com>
2024-03-11 12:26:25 +0100

    compare find_coins_greedy and find_min_coins

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 from find_coins_greedy import find_coins_greedy
 from find_min_coins import find_min_coins
+from measure_time import measure_time
+
+coins = [50, 25, 10, 5, 2, 1]
+
+# Тестування алгоритмів для суми 113 за умовою завдання
+amount = 113
+
+print("Результат для суми 113:")
+print("Жадібний алгоритм:", find_coins_greedy(coins, amount))
+print("Динамічне програмування:", find_min_coins(coins, amount))
+
+# Тестування алгоритмів на великих сумах
+amount = 10058
+
+greedy_result, greedy_time = measure_time(find_coins_greedy, coins, amount)
+dp_result, dp_time = measure_time(find_min_coins, coins, amount)
+
+print("Результат для суми 10058:")
+print("Жадібний алгоритм:", find_coins_greedy(coins, amount))
+print("Динамічне програмування:", find_min_coins(coins, amount))
+print("Час для жадібного алгоритму:", greedy_time)
+print("Час для динамічного програмування:", dp_time)
---
+++ measure_time.py
@@ -0,0 +1,9 @@
+import time
+
+# Функція для вимірювання часу виконання функції
+def measure_time(func, *args):
+    start_time = time.time()
+    result = func(*args)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    return result, execution_time
